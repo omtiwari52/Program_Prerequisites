3bd6205ffba250b5bad6766ffcb75cfb0712714b
omtiwari52 <tiwariom900@gmail.com>
2025-01-08 10:06:38 +0530

    Done

---
+++ AddTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddTwoNumbers{
+	public static void main(String[] args){
+		Scanner sc =  new Scanner(System.in);
+		System.out.print("Enter The Number One:- ");
+		int a = sc.nextInt();
+		System.out.print("Enter The Number Two :- ");
+		int b = sc.nextInt();
+		System.out.print("Sum Of The Two Number Is"+(a+b));
+
+	}
+}
