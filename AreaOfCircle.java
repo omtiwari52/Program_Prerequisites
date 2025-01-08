5918d612172c492de866aa3239e472720b8e2234
omtiwari52 <tiwariom900@gmail.com>
2025-01-08 10:15:48 +0530

    Done

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AreaOfCircle{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter The Radius Of The Circle:- ");
+		int radius = sc.nextInt();
+		System.out.print("Area Of The Circle Is:-  " + (3.14 * radius * radius));
+
+	}
+}
