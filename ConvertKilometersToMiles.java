2c5eff6d369f5a90b2da244f1c2f8851cf3123f4
omtiwari52 <tiwariom900@gmail.com>
2025-01-08 10:36:17 +0530

    Done

---
+++ ConvertKilometersToMiles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ConvertKilometersToMiles{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Kilometers :- ");
+		int km = sc.nextInt();
+				
+
+		System.out.print(km + " is equal to :- " + (km * 0.62137) + " miles");
+
+	}
+	
+}
